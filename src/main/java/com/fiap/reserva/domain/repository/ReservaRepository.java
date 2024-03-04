f4e544da23dbaa0bd5ec9bed04944be7a56dbf5b
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-03-03 21:56:02 -0300

    Adicionada Excessao de Business a ReservaRepository.java

--- src/main/java/com/fiap/reserva/domain/repository/ReservaRepository.java
+++ src/main/java/com/fiap/reserva/domain/repository/ReservaRepository.java
@@ -5,15 +5,14 @@ import java.util.List;
 import com.fiap.reserva.domain.entity.Reserva;
 import com.fiap.reserva.domain.entity.Restaurante;
 import com.fiap.reserva.domain.entity.Usuario;
+import com.fiap.reserva.domain.exception.BusinessException;
 
 public interface ReservaRepository {
-    List<Reserva> buscarTodasPor(Restaurante restaurante);
-    List<Reserva> buscarTodasPor(Usuario usuario);
-    List<Reserva> buscarTodasPor(Reserva reserva);
-    Reserva buscar(Reserva reserva);
-    Integer obterLotacaoaReserva(Reserva reserva);
-    Reserva criar(Reserva reserva);
-    Reserva alterar(Reserva reserva);
+    List<Reserva> buscarTodasPor(Restaurante restaurante) throws BusinessException;
+    List<Reserva> buscarTodasPor(Usuario usuario) throws BusinessException;
+    List<Reserva> buscarTodasPor(Reserva reserva) throws BusinessException;
+    Reserva buscarPor(Reserva reserva) throws BusinessException;
+    Reserva criar(Reserva reserva) throws BusinessException;
+    Reserva alterar(Reserva reserva) throws BusinessException;
     void excluir(Reserva reserva);
-   
 }
