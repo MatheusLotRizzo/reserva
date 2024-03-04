0fcc5ff09176ddd1ca1d10023d39992e65cff457
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-03-03 21:49:09 -0300

    Alteração script base sql chaves primarias alteradas para as chaves usadas no negocio

--- src/main/resources/inith2.sql
+++ src/main/resources/inith2.sql
@@ -1,30 +1,30 @@
 create table tb_reserva (
-    rowid bigint auto_increment primary key,
-    cd_usuario bigint not null,
-    cd_restaurante bigint not null,
-    dt_hr_reserva TIMESTAMP,
-    qt_lugares numeric,
-    ds_status varchar
+    rowid bigint auto_increment,
+    cd_numero_reserva uuid primary key,
+    cd_usuario varchar not null,
+    cd_restaurante varchar not null,
+    dt_hr_reserva TIMESTAMP not null,
+    ds_status varchar not null
 );
 
 create table tb_usuario (
-    rowid bigint auto_increment primary key,
+    rowid bigint auto_increment not null,
     nm_usuario varchar not null,
     ic_telefone varchar not null,
-    ic_email varchar unique not null
+    ic_email varchar primary key
 );
 
 create table tb_avaliacao (
     rowid bigint auto_increment primary key,
-    cd_usuario bigint not null,
-    cd_restaurante bigint not null,
+    cd_usuario varchar not null,
+    cd_restaurante varchar not null,
     qt_pontos int,
     ds_comentario varchar
 );
 
 create table tb_restaurante (
-    rowid bigint auto_increment primary key,
-    cd_cnpj varchar unique,
+    rowid bigint auto_increment,
+    cd_cnpj varchar primary key,
     nm_restaurante varchar,
     ic_email varchar,
     ds_tipo_cozinha varchar
@@ -32,7 +32,7 @@ create table tb_restaurante (
 
 create table tb_restaurante_endereco(
     rowid bigint auto_increment primary key,
-    cd_restaurante bigint not null,
+    cd_restaurante varchar not null,
     cd_cep varchar,
     ds_numero varchar,
     ds_complemento varchar,
@@ -43,26 +43,26 @@ create table tb_restaurante_endereco(
 
 create table tb_restaurante_horarios(
     rowid bigint auto_increment primary key,
-    cd_restaurante bigint not null,
+    cd_restaurante varchar not null,
     hr_abertura TIME not null,
     hr_fechamento TIME not null
 );
 
 
 ALTER TABLE tb_reserva ADD CONSTRAINT FK_USUARIO_RESERVA 
-FOREIGN KEY(cd_usuario) REFERENCES tb_usuario(rowid);
+FOREIGN KEY(cd_usuario) REFERENCES tb_usuario(ic_email);
 
 ALTER TABLE tb_reserva ADD CONSTRAINT FK_RESTAURANTE_RESERVA 
-FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(rowid);
+FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(cd_cnpj);
 
 ALTER TABLE tb_avaliacao ADD CONSTRAINT FK_USUARIO_AVALIACAO 
-FOREIGN KEY(cd_usuario) REFERENCES tb_usuario(rowid);
+FOREIGN KEY(cd_usuario) REFERENCES tb_usuario(ic_email);
 
 ALTER TABLE tb_avaliacao ADD CONSTRAINT FK_RESTAURANTE_AVALIACAO 
-FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(rowid);
+FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(cd_cnpj);
 
 ALTER TABLE tb_restaurante_endereco ADD CONSTRAINT FK_RESTAURANTE_ENDERECO 
-FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(rowid);
+FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(cd_cnpj);
 
 ALTER TABLE tb_restaurante_horarios ADD CONSTRAINT FK_RESTAURANTE_HORARIOS_RESTAURANTE
-FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(rowid);
+FOREIGN KEY(cd_restaurante) REFERENCES tb_restaurante(cd_cnpj);
