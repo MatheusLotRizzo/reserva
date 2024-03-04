dc36879095ba1d50acf1d91e9c5d0dd8766edf88
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-03-04 00:30:20 -0300

    Refactor ReservaDto.java

--- src/main/java/com/fiap/spring/Controller/Dto/ReservaDto.java
+++ src/main/java/com/fiap/spring/Controller/Dto/ReservaDto.java
@@ -1,12 +1,28 @@
 package com.fiap.spring.Controller.Dto;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.fiap.reserva.domain.entity.Reserva;
+import com.fiap.reserva.domain.entity.Restaurante;
+import com.fiap.reserva.domain.entity.SituacaoReserva;
+import com.fiap.reserva.domain.entity.Usuario;
+import com.fiap.reserva.domain.exception.BusinessException;
 
 public record ReservaDto(
-        String emailUsuario,
-        String cnpjRestaurante,
-        LocalDateTime dataHora,
-        int quantidadeLugares,
-        String status
+    UUID numeroReserva,
+    String emailUsuario,
+    String cnpjRestaurante,
+    LocalDateTime dataHora,
+    SituacaoReserva statusReserva       
 ) {
+    public Reserva toEntity() throws BusinessException{
+        return new Reserva(
+            numeroReserva,
+            new Usuario(emailUsuario), 
+            new Restaurante(cnpjRestaurante), 
+            dataHora, 
+            statusReserva
+        );
+    }
 }
