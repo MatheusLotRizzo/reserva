29a52c5eabbf995a51b92e8e142ddc7099a9e9f0
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-03-11 01:20:29 -0300

    Add metodo toEntity em UsuarioDto

--- src/main/java/com/fiap/spring/Controller/Dto/UsuarioDto.java
+++ src/main/java/com/fiap/spring/Controller/Dto/UsuarioDto.java
@@ -1,8 +1,14 @@
 package com.fiap.spring.Controller.Dto;
 
+import com.fiap.reserva.domain.entity.Usuario;
+import com.fiap.reserva.domain.exception.BusinessException;
+
 public record UsuarioDto(
         String nome,
         String email,
         String celular
 ) {
+	public Usuario toEntity() throws BusinessException {
+		return new Usuario(nome, email, celular);
+	}
 }
