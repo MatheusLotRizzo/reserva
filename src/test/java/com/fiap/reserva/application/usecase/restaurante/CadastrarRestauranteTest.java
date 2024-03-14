b4acf12a3a89aea6a4548fd12770a498d6621d68
Nicole Bidigaray <nicolenatsume@gmail.com>
2024-03-14 01:32:15 -0300

    Add e corrigido testes unitários do usecase restaurante CadastrarRest

--- src/test/java/com/fiap/reserva/application/usecase/restaurante/CadastrarRestauranteTest.java
+++ src/test/java/com/fiap/reserva/application/usecase/restaurante/CadastrarRestauranteTest.java
@@ -1,47 +1,58 @@
 package com.fiap.reserva.application.usecase.restaurante;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import com.fiap.reserva.domain.entity.Restaurante;
 import com.fiap.reserva.domain.exception.BusinessException;
 import com.fiap.reserva.domain.repository.RestauranteRepository;
 import com.fiap.reserva.domain.vo.CnpjVo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-@ExtendWith(MockitoExtension.class)
 class CadastrarRestauranteTest {
 
     @Mock
-    private RestauranteRepository restauranteRepository;
+    private RestauranteRepository repository;
 
     @InjectMocks
     private CadastrarRestaurante cadastrarRestaurante;
 
-    private Restaurante restaurante;
+    private AutoCloseable autoCloseable;
 
     @BeforeEach
-    void setUp() throws BusinessException {
-        // Configuração do restaurante de teste
-        restaurante = new Restaurante(new CnpjVo("12345678901234"), "Restaurante Teste");
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void naoDeveCadastrarRestauranteQuandoNulo() {
+        final Throwable throwable = assertThrows(BusinessException.class,
+                () -> cadastrarRestaurante.executar(null));
+
+        assertEquals("Restaurante é obrigatório", throwable.getMessage());
+        verifyNoInteractions(repository);
     }
 
     @Test
     void deveCadastrarRestauranteComSucesso() throws BusinessException {
-        // Configura o mock para retornar o restaurante quando o método cadastrar for chamado
-//        when(restauranteRepository.cadastrar(any(Restaurante.class))).thenReturn(restaurante);
-//
-//        // Executa o método executar
-//        Restaurante resultado = cadastrarRestaurante.executar(restaurante);
-//
-//        // Verifica se o resultado é o esperado
-//        assertNotNull(resultado, "O resultado não deve ser nulo.");
-//        assertEquals("Restaurante Teste", resultado.getNome(), "O nome do restaurante deve ser igual ao esperado.");
-//
-//        // Verifica se o método cadastrar foi chamado no repositório
-//        verify(restauranteRepository).cadastrar(restaurante);
+        CnpjVo cnpj = new CnpjVo("12345678901234");
+        Restaurante restaurante = new Restaurante(new CnpjVo("12345678901234"), "Restaurante Teste");
+
+        when(repository.cadastrar(restaurante)).thenReturn(restaurante);
+
+        Restaurante resultado = cadastrarRestaurante.executar(restaurante);
+
+        assertNotNull(resultado);
+        verify(repository).cadastrar(restaurante);
     }
 }
