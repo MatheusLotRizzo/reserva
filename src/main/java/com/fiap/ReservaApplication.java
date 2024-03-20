8feba908ff541f50970b51cc73d24daaf597d928
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-03-19 21:47:49 -0300

    Remoçao método desnecessario em ReservaApplication

--- src/main/java/com/fiap/ReservaApplication.java
+++ src/main/java/com/fiap/ReservaApplication.java
@@ -1,25 +1,9 @@
 package com.fiap;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.fiap.reserva.application.usecase.reserva.CadastrarReserva;
-import com.fiap.reserva.domain.entity.Reserva;
-import com.fiap.reserva.domain.entity.Restaurante;
-import com.fiap.reserva.domain.entity.SituacaoReserva;
-import com.fiap.reserva.domain.entity.TipoCozinha;
-import com.fiap.reserva.domain.entity.Usuario;
-import com.fiap.reserva.domain.exception.BusinessException;
-import com.fiap.reserva.domain.repository.ReservaRepository;
-import com.fiap.reserva.infra.jdbc.DataBaseConnection;
-import com.fiap.reserva.infra.jdbc.reserva.ReservaRepositoryImpl;
-import com.fiap.reserva.infra.jdbc.restaurante.RestauranteRepositoryImpl;
-import com.fiap.reserva.infra.jdbc.usuario.UsuarioRepositoryImpl;
 
 @SpringBootApplication
 public class ReservaApplication {
@@ -28,25 +12,4 @@ public class ReservaApplication {
 		SpringApplication.run(ReservaApplication.class, args);
 	}
 
-	void insereReservaNoBanco() throws SQLException, BusinessException {
-		Connection connection = DataBaseConnection.conectar();
-		ReservaRepository reservaRepository = new ReservaRepositoryImpl(connection);
-		CadastrarReserva cadastrarReserva = new CadastrarReserva(reservaRepository);
-		
-		final Usuario usuario = new Usuario("Weslei","fualno@com.br","");
-		new UsuarioRepositoryImpl(connection).cadastrar(usuario);
-
-		final Restaurante restaurante = new Restaurante("12345678900000", "nome_restaurante", null, null, 100, TipoCozinha.BRASILEIRA);
-		new RestauranteRepositoryImpl(connection).cadastrar(restaurante);
-		final Reserva reserva = new Reserva(
-			UUID.randomUUID(), 
-			usuario,
-			restaurante,
-			LocalDateTime.now().plusHours(1),
-			SituacaoReserva.RESERVADO);
-
-		cadastrarReserva.executar(reserva);
-
-		connection.close();
-	}
 }
