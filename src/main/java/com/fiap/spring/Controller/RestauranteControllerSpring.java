43e88fa1ed660ec862c0dc7aa40f4b3ea5a8d2c2
Nicole Bidigaray <nicolenatsume@gmail.com>
2024-03-22 18:15:02 -0300

    Correções efetuadas no RestauranteControllerSpring

--- src/main/java/com/fiap/spring/Controller/RestauranteControllerSpring.java
+++ src/main/java/com/fiap/spring/Controller/RestauranteControllerSpring.java
@@ -1,5 +1,9 @@
 package com.fiap.spring.Controller;
 
+import com.fiap.spring.infra.Utils;
+import com.fiap.spring.swagger.annotations.ApiResponseSwaggerCreate;
+import com.fiap.spring.swagger.annotations.ApiResponseSwaggerNoContent;
+import com.fiap.spring.swagger.annotations.ApiResponseSwaggerOk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fiap.reserva.application.controller.RestauranteControllerApplication;
 import com.fiap.spring.Controller.Dto.RestauranteDto;
 
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Restaurante", description = "Restaurante que recebera as reservas")
@@ -32,114 +31,49 @@ public class RestauranteControllerSpring {
 	@Autowired
     private RestauranteControllerApplication restauranteController;
 
-    @Operation(summary = "Cria um restaurante")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Sucesso",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RestauranteDto.class, description = "Restaurante")) }),
-            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)) }),
-    })
+
     @PostMapping
+    @Operation(summary = "Cria um restaurante")
+    @ApiResponseSwaggerCreate
     public ResponseEntity<?> criarRestaurante(@RequestBody RestauranteDto restauranteDto){
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(restauranteController.cadastrar(restauranteDto));
-        } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+        return Utils.response(HttpStatus.CREATED, () -> restauranteController.cadastrar(restauranteDto));
     }
 
-    @Operation(summary = "Altera um restaurante")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Sucesso",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RestauranteDto.class, description = "Restaurante")) }),
-            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)) }),
-    })
     @PutMapping
+    @Operation(summary = "Altera um restaurante")
+    @ApiResponseSwaggerCreate
     public ResponseEntity<?> alterarRestaurante(@RequestBody RestauranteDto restauranteDto ){
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(restauranteController.alterar(restauranteDto));
-        } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+        return Utils.response(HttpStatus.CREATED, () -> restauranteController.alterar(restauranteDto));
     }
 
-    @Operation(summary = "Deleta um restaurante")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Sucesso",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RestauranteDto.class, description = "Restaurante")) }),
-            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)) }),
-    })
     @DeleteMapping("/{cnpj}")
-    public ResponseEntity<?> excluirRestaurante(@PathVariable @ApiParam(value = "Cnpj", example = "11 caracteres alfanumericos")
-                                                    String cnpj ){
-        try{
+    @Operation(summary = "Deleta um restaurante")
+    @ApiResponseSwaggerNoContent
+    public ResponseEntity<?> excluirRestaurante(@PathVariable @ApiParam(value = "Cnpj", example = "11 caracteres alfanumericos") String cnpj ){
+        return Utils.response(HttpStatus.NO_CONTENT, () -> {
             restauranteController.excluir(cnpj);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Sucesso");
-        } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+            return null;
+        });
     }
 
-    @Operation(summary = "Busca restaurante")
-    @ApiOperation("Busca restaurante por cnpj")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Sucesso",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RestauranteDto.class, description = "Restaurante")) }),
-            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)) }),
-    })
     @GetMapping("/{cnpj}")
-    public ResponseEntity<?> buscarPorCnpj(@PathVariable @ApiParam(value = "Cnpj", example = "11 caracteres alfanumericos")
-                                               String cnpj) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(
-                    restauranteController.getBuscarPor(cnpj)
-            );
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+    @Operation(summary = "Busca restaurante")
+    @ApiResponseSwaggerOk
+    public ResponseEntity<?> buscarPorCnpj(@PathVariable @ApiParam(value = "Cnpj", example = "11 caracteres alfanumericos") String cnpj) {
+        return Utils.response(HttpStatus.OK, () -> restauranteController.getBuscarPor(cnpj));
     }
 
-    @Operation(summary = "Busca restaurante")
-    @ApiOperation("Busca restaurante por nome")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Sucesso",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RestauranteDto.class, description = "Restaurante")) }),
-            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)) }),
-    })
     @GetMapping("/{nome}")
-    public ResponseEntity<?> buscarPorNome(@PathVariable @ApiParam(value = "nome", example = "nome do restaurante")
-                                               String nome) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(
-                    restauranteController.getBuscarPorNome(nome)
-            );
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+    @Operation(summary = "Busca restaurante por nome")
+    @ApiResponseSwaggerOk
+    public ResponseEntity<?> buscarPorNome(@PathVariable @ApiParam(value = "nome", example = "nome do restaurante") String nome) {
+        return Utils.response(HttpStatus.OK, () -> restauranteController.getBuscarPorNome(nome));
     }
 
-    @Operation(summary = "Busca restaurante")
-    @ApiOperation("Busca restaurante por tipo de cozinha")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Sucesso",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RestauranteDto.class, description = "Restaurante")) }),
-            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)) }),
-    })
     @GetMapping("/{tipoCozinha}")
-    public ResponseEntity<?> buscarPorTipoCozinha(@PathVariable
-                                                      @ApiParam(value = "Status da entidade", example = "ITALIANA",  allowableValues = "ITALIANA,JAPONESABRASILEIRA,FRANCESA,MEXICANA,VEGANA,VEGETARIANA")
-                                                      String tipoCozinha) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(
-                    restauranteController.getBuscarPorTipoCozinha(tipoCozinha)
-            );
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+    @Operation(summary = "Busca restaurante por tipo de cozinha")
+    @ApiResponseSwaggerOk
+    public ResponseEntity<?> buscarPorTipoCozinha(@PathVariable @ApiParam(value = "Status da entidade", example = "ITALIANA",  allowableValues = "ITALIANA,JAPONESABRASILEIRA,FRANCESA,MEXICANA,VEGANA,VEGETARIANA") String tipoCozinha) {
+        return Utils.response(HttpStatus.OK, () -> restauranteController.getBuscarPorTipoCozinha(tipoCozinha));
     }
 }
