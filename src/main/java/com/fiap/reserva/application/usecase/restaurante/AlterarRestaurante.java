cd98d4c639ee133b95c03c1801caeaa01cc86423
Nicole Bidigaray <nicolenatsume@gmail.com>
2024-03-24 22:11:23 -0300

    Retirado o try catch de BuscarRestaurante

--- src/main/java/com/fiap/reserva/application/usecase/restaurante/AlterarRestaurante.java
+++ src/main/java/com/fiap/reserva/application/usecase/restaurante/AlterarRestaurante.java
@@ -2,7 +2,6 @@ package com.fiap.reserva.application.usecase.restaurante;
 
 import com.fiap.reserva.domain.entity.Restaurante;
 import com.fiap.reserva.domain.exception.BusinessException;
-import com.fiap.reserva.domain.exception.EntidadeNaoEncontrada;
 import com.fiap.reserva.domain.repository.RestauranteRepository;
 
 public class AlterarRestaurante {
@@ -17,14 +16,6 @@ public class AlterarRestaurante {
         if (restaurante == null) {
             throw new BusinessException("Restaurante é obrigatorio");
         }
-
-        try {
-            new BuscarRestaurante(repository).getRestaurantePor(restaurante.getCnpj());
-            // A lógica para alterar o restaurante segue aqui, assumindo que o restaurante foi encontrado
-            return repository.alterar(restaurante);
-        } catch (EntidadeNaoEncontrada e) {
-            // Lançar uma exceção específica para a operação de alteração
-            throw new EntidadeNaoEncontrada("Restaurante não pode ser alterado, pois não foi encontrado");
-        }
+        return repository.alterar(restaurante);
     }
 }
--- src/test/java/com/fiap/reserva/application/usecase/restaurante/AlterarRestauranteTest.java
+++ src/test/java/com/fiap/reserva/application/usecase/restaurante/AlterarRestauranteTest.java
@@ -3,7 +3,6 @@ package com.fiap.reserva.application.usecase.restaurante;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import com.fiap.reserva.domain.exception.EntidadeNaoEncontrada;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -44,34 +43,16 @@ class AlterarRestauranteTest {
     }
 
     @Test
-    void naoDeveAlterarRestauranteSeNaoEncontrado() throws BusinessException {
-        CnpjVo cnpj = new CnpjVo("12345678901234");
-        Restaurante restaurante = new Restaurante(cnpj, "Restaurante Teste");
-        AlterarRestaurante alterarRestaurante = new AlterarRestaurante(repository);
-
-        when(repository.buscarPorCnpj(cnpj)).thenReturn(null);
-
-        final Throwable throwable = assertThrows(EntidadeNaoEncontrada.class, () -> alterarRestaurante.executar(restaurante));
-
-        assertEquals("Restaurante não pode ser alterado, pois não foi encontrado", throwable.getMessage());
-        verify(repository).buscarPorCnpj(cnpj);
-        verifyNoMoreInteractions(repository);
-    }
-
-    @Test
     void deveAlterarRestauranteExistente() throws BusinessException {
         CnpjVo cnpj = new CnpjVo("12345678901234");
-        Restaurante restauranteExistente = new Restaurante(cnpj, "Restaurante Existente");
         Restaurante restauranteAlterado = new Restaurante(cnpj, "Restaurante Alterado");
 
-        when(repository.buscarPorCnpj(cnpj)).thenReturn(restauranteExistente);
         when(repository.alterar(restauranteAlterado)).thenReturn(restauranteAlterado);
 
-        Restaurante resultado = new AlterarRestaurante(repository).executar(restauranteAlterado);
+        Restaurante resultado = alterarRestaurante.executar(restauranteAlterado);
 
         assertNotNull(resultado);
         assertEquals(restauranteAlterado, resultado);
-        verify(repository).buscarPorCnpj(cnpj);
         verify(repository).alterar(restauranteAlterado);
     }
 }
